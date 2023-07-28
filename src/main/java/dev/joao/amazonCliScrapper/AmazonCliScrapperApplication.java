4bc71309d0cd3da2fa307b82ceca0991d5008443
Joao Henrique <joaoibitira@gmail.com>
2023-07-27 23:28:05 -0300

    Initial commit

---
+++ src/main/java/dev/joao/amazonCliScrapper/AmazonCliScrapperApplication.java
@@ -0,0 +1,13 @@
+package dev.joao.amazonCliScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCliScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonCliScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joao/amazonCliScrapper/AmazonCliScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joao.amazonCliScrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonCliScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
